bac4d8b46729db15d6810efa53f2834263ed4adc
William <wmcabangon@gmail.com>
2017-09-20 16:38:25 +0100

    can filter via tag

--- app.rb
+++ app.rb
@@ -19,4 +19,10 @@ class BookmarkManager < Sinatra::Base
     link.save
     redirect '/links'
   end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
 end
