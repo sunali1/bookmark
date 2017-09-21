452158dd3f913750f55dac73a7b089ca3a60d393
William <wmcabangon@gmail.com>
2017-09-21 10:13:23 +0100

    rearranged folders

---
+++ app/app.rb
@@ -0,0 +1,29 @@
+ENV["RACK_ENV"] ||= "development"
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
+class BookmarkManager < Sinatra::Base
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  post '/links' do
+   link = Link.create(url: params[:url], title: params[:title])
+   params[:tags].split.each do |tag|
+     link.tags << Tag.first_or_create(name: tag)
+   end
+   link.save
+   redirect to('/links')
+ end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+end
