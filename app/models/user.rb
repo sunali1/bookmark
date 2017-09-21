d0221d26439195d581ad44a2b7de64425cd4b153
William <wmcabangon@gmail.com>
2017-09-21 15:56:03 +0100

    checks for password confirmation before creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,23 @@ require 'bcrypt'
 class User
   include DataMapper::Resource
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+# validates_confirmation_of is a DataMapper method
+# provided especially for validating confirmation passwords!
+# The model will not save unless both password
+# and password_confirmation are the same
+# read more about it in the documentation
+# http://datamapper.org/docs/validations.html
+  validates_confirmation_of :password
+
   property :id, Serial
   property :email, String
   property :password_digest, Text
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,4 +4,21 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, test@test.com')
     expect(User.first.email).to eq('test@test.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    # again it's questionable whether we should be testing the model at this
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'test@test.com',
+              password: 'test123',
+              password_confirmation: 'test123')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
 end
